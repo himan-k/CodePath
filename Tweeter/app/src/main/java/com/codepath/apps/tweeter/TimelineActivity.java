48078832a547023ddece24b24c30d8930d44d407
Himanshu Kale <himanshukale@hotmail.com>
2015-02-21 17:49:02 -0800

    tabs

--- Tweeter/app/src/main/java/com/codepath/apps/tweeter/TimelineActivity.java
+++ Tweeter/app/src/main/java/com/codepath/apps/tweeter/TimelineActivity.java
@@ -1,17 +1,30 @@
-package com.codepath.apps.restclienttemplate;
+package com.codepath.apps.tweeter;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.tweeter.models.HomeTimelineFragment;
+import com.codepath.apps.tweeter.models.MentionsFragment;
 
 public class TimelineActivity extends ActionBarActivity {
-
+    private FragmentTabHost tabHost;
+    private HomeTimelineFragment timelineFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+        // get the tab host
+        tabHost = (FragmentTabHost) findViewById(R.id.tabHost);
+        tabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        // add the two tabs we want, referencing the views that include
+        // the appropriate fragments
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Home"), HomeTimelineFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Mentions"), MentionsFragment.class, null);
     }
 
 
